c591f07545204292179bab04b604d09c56c1c279
Willem Larsen <willemlarsen@gmail.com>
2014-06-17 13:10:15 -0700

    add web form

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,33 @@ var Triangle = {
 
 };
 
+$(document).ready(function () {
+	$('form#new-triangle').submit(function(event) {
+		event.preventDefault();
+
+		var inputSideOne = parseInt($('input#new-triangle-side1').val(), 10);
+		var inputSideTwo = parseInt($('input#new-triangle-side2').val(), 10);
+		var inputSideThree = parseInt($('input#new-triangle-side3').val(), 10);
+
+		var newTriangle = Object.create(Triangle);
+			newTriangle.sideA = inputSideOne;
+			newTriangle.sideB = inputSideTwo;
+			newTriangle.sideC = inputSideThree;
+
+
+		if(!newTriangle.valid()) {
+			return alert("Sorry, that's not a valid triangle.");
+		} else if(newTriangle.type() === "equilateral") {
+	   		$('ul#equilateral').append("<li>" + inputSideOne + ", " + inputSideTwo + ", " + inputSideThree + "</li>");
+	  } else if (newTriangle.type() === "isosceles") {
+	  		$('ul#isosceles').append("<li>" + inputSideOne + ", " + inputSideTwo + ", " + inputSideThree + "</li>");
+	  } else {
+	  		$('ul#scalene').append("<li>" + inputSideOne + ", " + inputSideTwo + ", " + inputSideThree + "</li>");
+	  }
+	    
+	});
+});
+
 
 
 
