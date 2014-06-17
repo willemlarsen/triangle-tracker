bcc761ecc6f8ef04bafef97035e2df2a32f61e6c
Willem Larsen <willemlarsen@gmail.com>
2014-06-17 09:52:30 -0700

    pass second test for unworkable triangles

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,19 @@
 describe('TriangleTracker', function () {
 		describe('triangleTest', function () {
-			it('returns a boolean if a given potential triangle would work', function() {
+			it('returns a true if a given potential triangle would work', function() {
 				var exampleTriangleOne = Object.create(TriangleTracker);
 					exampleTriangleOne.sideA = 3;
 					exampleTriangleOne.sideB = 3;
 					exampleTriangleOne.sideC = 3;
 					exampleTriangleOne.triangleTest().should.equal(true);
+				
+			});
+			it('returns a false if a given potential triangle would not work', function () {
+			var exampleTriangleTwo = Object.create(TriangleTracker);
+					exampleTriangleTwo.sideA = 4;
+					exampleTriangleTwo.sideB = 5;
+					exampleTriangleTwo.sideC = 10;
+					exampleTriangleTwo.triangleTest().should.equal(false);
 			});
 		});
 });
